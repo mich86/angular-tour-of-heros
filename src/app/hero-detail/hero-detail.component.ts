cbfd8739dbb81c297b09fafac19805a37aefcb8a
Michelle Campbell <michelle.campbell086@gmail.com>
2019-10-13 12:30:42 +0100

    completed tutorial

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -11,12 +11,12 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  hero: Hero;
+  @Input() hero: Hero;
 
   constructor(
-    private route: ActivatedRoute, //holds info about the route of HeroDetailComponent
-    private heroService: HeroService, //gets data from the remote server, this component will use it to get hero-to-display
-    private location: Location //interacting with browser
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
   ) {}
 
   ngOnInit(): void {
@@ -24,11 +24,15 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = +this.route.snapshot.paramMap.get('id'); //snapshot static image of route info. paraMap is a dictionary of route parameter values from the URL. + converts the string to a number
+    const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 }
